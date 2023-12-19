d47216a5be9c1f16579afd605f6c667539712153
Omanshb <73659149+Omanshb@users.noreply.github.com>
2023-12-19 09:23:34 -0800

    buttons and username input

--- letterboxd-recs/pages/index.js
+++ letterboxd-recs/pages/index.js
@@ -1,17 +1,32 @@
-import { Image, Text, Box, Center } from '@chakra-ui/react';
+import { FormControl, FormErrorMessage, Image, Input, Text, Textarea, Box, Center, Button } from '@chakra-ui/react';
+import { useState } from 'react';
 
 export default function Home() {
+
+  const [user, setUser] = useState("");
+  const [userError, setUserError] = useState("");
+  const handleUserChange = (event) => {
+    setUser(event.target.value);
+  };
+
   return (
     <>
-      <Box bg="black" minH="100vh"> {/* Set background color to black */}
+      <Box bg="black" minH="100vh">
         <Center>
           <Image src="/logo.png" alt="Letterboxd Recs Logo" w="500px" l="240" />
         </Center>
         <Center>
           <Box maxW={{ base: "80%", xl: "1000px" }} textAlign="center">
             <Text mt="40px" color="white" fontSize="1em" fontFamily="Courier New, monospace" fontWeight="thin">
-              Welcome fellow movie enjoyers! As you know, LetterBoxd is a social media platform for users to share their reviews for movies they've watched and discover new films. However, the platform currently recommends movies based on popularity rather than personalization. This is where this project comes in! Enter your LetterBoxd username, and we will provide you with a list of personalized recommendations that you can then directly add to your Letterboxd watchlist. Additionally, the chatbot "Friday" is available to assist you in the movie searching process. Enjoy!
+              Welcome film enthusiast! Letterboxd is a project for users to discover and review new films. However, the platform doesn't use provide personal recommendations and only shows movies based on popularity. This tool aims to fix that and ease your movie search project by providing user-based recommendations and a personal movie finding chatbot called "Friday". You can additionally add the movies directly to your watchlist on LetterBoxd. Enjoy and leave feedback!
             </Text>
+            <FormControl mt="10">
+              <Input placeholder="enter in your Letterboxd username" value={user} onChange={handleUserChange} textAlign="center" mt="50px" color="white" fontSize="1em" fontFamily="Courier New, monospace" fontWeight="thin" width="450px" />
+            </FormControl>
+            <Box mt="10">
+              <Button mr="5" colorScheme="green" variant="outline" width="200px">Get Recommendations</Button>
+              <Button colorScheme="green" width="200px">Friday Chatbot</Button>
+            </Box>
           </Box>
         </Center>
       </Box>
